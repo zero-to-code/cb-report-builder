81a8827de682867a3a524aa9b71e9dd6e67284a3
Kevin Moore <k.james.moore@gmail.com>
2018-10-05 13:02:31 -0700

    JSON-powered Report Generator

--- controller.js
+++ controller.js
@@ -1,41 +1,47 @@
+// Determine which report needs to be loaded from URL
+// query parameters.
+// i.e. localhost:8000/report.html?report=transportation means
+// to get the report data from transportation.json
+// Defaults to communication.json.
+
+const parameters = window.location.search;
+const reportName = parameters.split("=")[1] || "communication";
+
 // Fetch data, then feed data into callback function
-
-
-
-//
-$.getJSON("/data/communication.json", callback);
+const reportPath = "/data/" + reportName + ".json";
+$.getJSON(reportPath, createReport);
 
 //
 // Supporting code
 //
 
 // master callback
-function callback(json) {
+function createReport(json) {
   createTitle(json.meta);
   createMain(json.data);
 }
 
 // create Title section
-function createTitle(data) {
-  $('.MunicipalBody').html(data.municipal_body);
-  $('.DocumentTitle').html(data.title);
+function createTitle(metadata) {
+  $('.Meta__Government').html(metadata.municipal_body);
+  $('.Meta__Title').html(metadata.title);
 }
 
-// create report section
-function createMain(data) {
-  for (let i = 0; i < data.length; i++) {
-    const $reportItem = createReportItem(data[i]);
-    $('.Report').append($reportItem);
+// create data section of report
+function createMain(rows) {
+  for (let i = 0; i < rows.length; i++) {
+    const $reportRow = createRow(rows[i]);
+    $('.Report').append($reportRow);
   }
 }
 
 // create individual items on report
-function createReportItem(data) {
-  const $reportItem = $("<div class='ReportItem'></div>");
-  const $label = $("<div class='ReportItem__Label'>" + data.label + ": </div>");
-  const $value = $("<div class='ReportItem__Value'>" + data.value + "</div>");
+function createRow(data) {
+  const $reportRow = $("<div class='Report__Row row'></div>");
+  const $label = $("<div class='Report__Label col-6'>" + data.label + ": </div>");
+  const $value = $("<div class='Report__Value col-6'><span>" + data.value + "<span></div>");
 
-  $reportItem.append($label, $value);
+  $reportRow.append($label, $value);
 
-  return $reportItem;
+  return $reportRow;
 }
